82500eba7ad15dcb7e2b1a20d8641fe902a35b52
priyanka7569 <kota.priyanka@mphasis.com>
2023-11-09 13:23:54 +0530

    this is my third b program

---
+++ Assignment 3b/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number to find whether it is +ve//-ve//even//odd");
+            int Number = int.Parse(Console.ReadLine());
+            if (Number > 0)
+            {
+                Console.WriteLine("Number is +ve \t" + Number);
+                if (Number % 2 == 0)
+                {
+                    Console.WriteLine("Even Number \t" + Number);
+                }
+                else
+                {
+                    Console.WriteLine("Odd Number \t" + Number);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Number is -ve \t" + Number);
+                if (Number % 2 == 0)
+                {
+                    Console.WriteLine("Even Number \t" + Number);
+                }
+                else
+                {
+                    Console.WriteLine("Odd Number \t" + Number);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
